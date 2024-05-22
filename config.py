a5145144edf8009d71b414f35f6373fef3dcf784
slavik321df <litvinvacheslav17@gmail.com>
2024-05-22 23:55:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 
 #BOT:
-TOKEN = "" # ТОКЕН БОТА
-ADMIN = 392192947 # ИД АДМИНА, узнать свой айди - @get_any_telegram_id_bot
+TOKEN = "7170572038:AAFC_8JZqwaxxKYe5seRebrr1wzfLPN_dt0" # ТОКЕН БОТА
+ADMIN = 5536338524 # ИД АДМИНА, узнать свой айди - @get_any_telegram_id_bot
 
 DIR = "" # Путь к файлу, если нужно
 
 #USER: # https://my.telegram.org/auth
-API_ID = 6623613
-API_HASH = "3504ada74bdd34a2e5bf510e1bfd6460"
+API_ID = 26491053
+API_HASH = "2c55d999c67926a7ca1d98a0f982caf3"
